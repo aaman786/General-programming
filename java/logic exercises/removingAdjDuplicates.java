519cfc754644e6ed357b3cba6c41bf7419391e5e
Aaman Satvilkar <88088113+aaman786@users.noreply.github.com>
2023-01-11 11:42:57 +0530

    Initial

--- java/logic exercises/removingAdjDuplicates.java
+++ java/logic exercises/removingAdjDuplicates.java
@@ -9,30 +9,35 @@ public class removingAdjDuplicates {
     }
 
     public static void main(String[] args) {
-        String original = "aabaarbarccrabmq";
-        char[] arrOrg = new char[original.length()];
-        arrOrg = original.toCharArray();
-
-        for (int i = 0; i < arrOrg.length; i++) {
-            for (int j = i + 1; j < arrOrg.length; j++) {
-
-                if (arrOrg[i] == arrOrg[j]) {
-                    System.out.println("com " + i + " " + arrOrg[i] + " and " + j + " " +
-                            arrOrg[j]);
-
-                    original = charRemoveAti(original, i);
-                    arrOrg[i] = charRemoveAti(original, i).toCharArray()[0];
-                    System.out.println("when at i : " + i + " -- " + original);
-                    original = charRemoveAt(original, j);
-                    arrOrg[j] = charRemoveAt(original, j).toCharArray()[0];
-                    System.out.println("when at j : " + j + " -- " + original);
-                    break;
-                }
-            }
-        }
-
-        // original = charRemoveAt(original, 02);
-        System.out.println(original);
-
+        var v = charRemoveAt("oky", 2);
+        System.out.println(v);
     }
+
+    // public static void main(String[] args) {
+    // String original = "aabaarbarccrabmq";
+    // char[] arrOrg = new char[original.length()];
+    // arrOrg = original.toCharArray();
+
+    // for (int i = 0; i < arrOrg.length; i++) {
+    // for (int j = i + 1; j < arrOrg.length; j++) {
+
+    // if (arrOrg[i] == arrOrg[j]) {
+    // System.out.println("com " + i + " " + arrOrg[i] + " and " + j + " " +
+    // arrOrg[j]);
+
+    // original = charRemoveAti(original, i);
+    // arrOrg[i] = charRemoveAti(original, i).toCharArray()[0];
+    // System.out.println("when at i : " + i + " -- " + original);
+    // original = charRemoveAt(original, j);
+    // arrOrg[j] = charRemoveAt(original, j).toCharArray()[0];
+    // System.out.println("when at j : " + j + " -- " + original);
+    // break;
+    // }
+    // }
+    // }
+
+    // // original = charRemoveAt(original, 02);
+    // System.out.println(original);
+
+    // }
 }
